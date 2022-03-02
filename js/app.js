fb619813606e4dbe8471bb5536d1fe837e5a9789
SShipon <nxshipon@gmail.com>
2022-03-02 13:37:15 +0630

    hey

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 const searchField = () => {
   const searchFieldInput = document.getElementById("search-field");
   const searchText = searchFieldInput.value;
@@ -20,27 +22,27 @@ const displaySearchResult = (phone) => {
   displaySearchPhoneResult.textContent = "";
   errorHandel.textContent = "";
   if (phone.length == 0) {
-    const error404 = document.createElement("div");
-    error404.innerHTML = `
-      <h1 class="text-center text-danger"> result Not Found 404 !!!</h1>
-      <p class="text-center text-danger">You find the right information !!!</p>
+    const error = document.createElement("div");
+    error.innerHTML = `
+      <h1 class="text-center text-danger">No Result found</h1>
+      <p class="text-center text-danger">Please Sir Try Again</p>
       `;
-    errorHandel.appendChild(error404);
+    errorHandel.appendChild(error);
   } else {
     const NewData = phone.slice(0, 20);
     NewData.forEach((data) => {
       const newDiv = document.createElement("div");
       newDiv.classList.add("col");
       newDiv.innerHTML = `
-        <div class="card h-100 shadow-lg p-4">
+        <div class="card h-100 mb-3 shadow-lg p-4">
                     <img src="${data.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">Brand: ${data.brand}</h5>
-                      <h5 class="card-title">Name: ${data.phone_name}</h5>
-                      <section id="phone-details">
-                      </section>
+                      <h4 class="card-title">Brand:${data.brand}</h4>
+                      <h6 class="card-title">Phone Name:<p>${data.phone_name}</p></h6>
+                      
+                       <a href="#phone-details"><button onclick="phoneDetails('${data.slug}')" class="btn btn-info text-white">Details</button></a>
 
-                       <button onclick="phoneDetails('${data.slug}')" class="btn btn-info text-white">Details</button>
+                      
                     </div>
                   </div>
         `;
@@ -49,7 +51,7 @@ const displaySearchResult = (phone) => {
     });
   }
 };
-//phone details Section
+// loded phone detels
 
 const phoneDetails = (phoneId) => {
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
@@ -58,7 +60,6 @@ const phoneDetails = (phoneId) => {
     .then((data) => displayPhonesDetails(data.data));
 };
 const displayPhonesDetails = (data) => {
-  // console.log(data);
   const phoneDetails = document.getElementById("phone-details");
   phoneDetails.textContent = "";
   const div = document.createElement("div");
@@ -70,27 +71,12 @@ const displayPhonesDetails = (data) => {
       <img src="${data.image}" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-8 col-sm-12">
-      <div id="others" class="card-body">
-        <h5 class="card-title">Brand: ${data.brand}</h5>
-        <p style="margin-bottom:-1px;" class="card-text text-bold">ChipSet: ${
-          data.mainFeatures.chipSet
-        }<br>
-        DisplaySize: ${data.mainFeatures.displaySize}
-        <br>
-        memory: ${data.mainFeatures.memory}</p>
-        <br>
-         Storage:${data.mainFeatures.storage}
-         <br>
-         Sensors: ${data.mainFeatures.sensors}
-         <h5 class="card-title">Release Date: ${
-           data.releaseDate ? data.releaseDate : "Not release date "
-         }</h5>
-         <p></p>
-         
-        <h5 id="text-content" onclick="othersDetails('${
-          data.slug
-        }')" class="text-primary">others: <i class="fas fa-angle-double-right"></i></h5>
-
+      <div id="others">
+        <h6>Brand: ${data.brand}</h6>
+        <h6>Name: ${data.name}</h6>
+        <h6> releaseDate: ${data.releaseDate}</h6>
+        <h5 id="text-content" onclick="othersDetails('${data.slug}')" class="text-primary">others: <i class="fas fa-angle-double-right"></i></h5>
+        
       </div>
     </div>
   </div>
@@ -99,25 +85,49 @@ const displayPhonesDetails = (data) => {
   phoneDetails.appendChild(div);
 };
 
+// load others details 
+
 const othersDetails = (phoneId) => {
   const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => phoneOthersDetails(data));
 };
+// display others details
+
 const phoneOthersDetails = (others) => {
   const moreOthersDetails = document.getElementById("others");
   const textContent = document.getElementById("text-content");
   textContent.style.display = "none";
   const div = document.createElement("div");
   div.innerHTML = `
-    <p style="margin-bottom:-1px;">GPS: ${others.data.others.GPS}</p>
-    <p style="margin-bottom:-1px;">NFC: ${others.data.others.NFC}</p>
-    <p style="margin-bottom:-1px;">Radio: ${others.data.others.Radio}</p>
-    <p style="margin-bottom:-1px;">Bluetooth: ${others.data.others.Bluetooth}</p>
-    <p style="margin-bottom:-1px;">USB: ${others.data.others.USB}</p>
-    <p style="margin-bottom:-1px;">WLAN: ${others.data.others.WLAN}</p>
-    
+    <h6 class="text-primary">Main Features Info</h6>
+    <h6>ChipSet: ${others.data.mainFeatures?.chipSet}</p>
+    <h6>Memory: ${others.data.mainFeatures?.memory}</h6>
+    <h6>DisplaySize: ${others.data.mainFeatures?.displaySize}</p>
+    <h6>DisplaySize: ${others.data.mainFeatures?.sensors}</p>
+    <div id="sensors-content"></div>
+    <h6 class="text-primary">Brand Others Info</h6>
+    <h6 style="margin-bottom:-1px;">Bluetooth: ${
+      others.data.others?.Bluetooth
+        ? others.data.others?.Bluetooth
+        : "No Bluetooth"
+    }</h6>
+    <h6 style="margin-bottom:-1px;">GPS: ${
+      others.data.others?.GPS ? others.data.others?.GPS : "No GPS"
+    }</h6>
+    <h6 style="margin-bottom:-1px;">NFC: ${
+      others.data.others?.NFC ? others.data.others?.NFC : "No NFC"
+    }</h6>
+    <h6 style="margin-bottom:-1px;">Radio: ${
+      others.data.others?.Radio ? others.data.others?.Radio : "No Radio"
+    }</h6>
+    <h6 style="margin-bottom:-1px;">USB: ${
+      others.data.others?.USB ? others.data.others?.USB : "No USB"
+    }</h6>
+    <h6 style="margin-bottom:-1px;">WLAN: ${
+      others.data.others?.WLAN ? others.data.others?.WLAN : "No WLAN"
+    }</h6>
     `;
   moreOthersDetails.appendChild(div);
 };
